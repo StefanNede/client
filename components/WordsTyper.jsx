b07f68c43214456163126ba554b588b39bdd54aa
Stefan Nedelcu <stefan.n@gmx.co.uk>
2024-02-12 23:54:43 +0000

    nearly finished phase 2

--- components/WordsTyper.jsx
+++ components/WordsTyper.jsx
@@ -22,7 +22,19 @@ const getElements = (letters) => {
     return lettersEls
 }
 
+const getPreviousLengths = (words, index) => {
+    let s = 0
+    for (let i = 0; i<index;i++) {
+        s += words[i].length
+        s++ // for the space separating the words
+    }
+    return s
+}
+
 export default function WordsTyper({ testLength }) {
+    const [wpm, setWPM] = useState(0)
+    const [accuracy, setAccuracy] = useState(0)
+
     const [text, setText] = useState("")
     let words = text.split(" ")
     let letters = getLetters(text)
@@ -35,6 +47,7 @@ export default function WordsTyper({ testLength }) {
 
     const [startTime, setStartTime] = useState(0)
     const [timeTaken, setTimeTaken] = useState(0)
+    const averageCharactersPerWord = 5
 
     useEffect(() => {
         setText(GetText(testLength))
@@ -48,110 +61,152 @@ export default function WordsTyper({ testLength }) {
 
     function checkDeleted(event) {
         // only on key up will the resulting text after the deletion be present
-        // on key down will see what was there before deletion making my system not work
         if (event.key === "Backspace" || event.key === "Delete") {
             // check what is currently in the input box because they could've done ctrl+backspace which deletes everything
             let wordTyped = event.target.value.trim() // this is the word left over after the backspace
-            // wordTyped = wordTyped.substring(0, wordTyped.length-1)
             const previousLengths = getPreviousLengths(words, index)
+            // add wordTyped.length to previousLengths to place letterIndex however much through the word the user is
             setLetterIndex(previousLengths + wordTyped.length)
-            // console.log(previousLengths, wordTyped, letterIndex)
 
             // change the displayed letters (green or red or white letters) to updated version
             for (let li = previousLengths+wordTyped.length; li < previousLengths + words[index].length; li++) {
                 if (letters[li] !== " ") { 
-                    lettersEls[li] = <div key={li} className="text-white opacity-50">{letters[li]}</div>
+                    lettersEls[li] = <div key={li} className="text-white text-lg opacity-50">{letters[li]}</div>
                 }
             }
         }
     }
 
     function checkCorrect(event) {
-        // console.log(letterIndex)
-        // console.log(letters)
         if (index == 0 && startTime === 0) {
+            // if on the first word and the timer hasn't been started start timer by recording start time
             let start = new Date();
             setStartTime(start.getTime());
         }
-        if (event.key === "Backspace" || event.key === "Delete") {
+
+        if (event.key === "Shift" || event.key === "CapsLock") {
+            return 
+        }
+
+        else if (event.key === "Backspace" || event.key === "Delete") {
             // check what is currently in the input box because they could've done ctrl+backspace which deletes everything
             let wordTyped = event.target.value.trim() // this is the word left over after the backspace
-            // wordTyped = wordTyped.substring(0, wordTyped.length-1)
+            // get lengths of all previous words to be able to update letterIndex correctly 
             const previousLengths = getPreviousLengths(words, index)
-            setLetterIndex(previousLengths + wordTyped.length)
-            // console.log(previousLengths, wordTyped, letterIndex)
+            // add wordTyped.length to previousLengths to place letterIndex however much through the word the user is
+            setLetterIndex(previousLengths + wordTyped.length) 
 
             // change the displayed letters (green or red or white letters) to updated version
             for (let li = previousLengths+wordTyped.length; li < previousLengths + words[index].length; li++) {
                 if (letters[li] !== " ") { 
-                    lettersEls[li] = <div key={li} className="text-white opacity-50">{letters[li]}</div>
+                    lettersEls[li] = <div key={li} className="text-white text-lg opacity-50">{letters[li]}</div>
                 }
             }
         }
+
         else if (event.key === " ") {
-            // console.log("space pressed")
+            // when spacebar pressed move on to the next word so update all the indexes 
             const wordTyped = event.target.value.trim()
+
             if (wordTyped == words[index]) {
+                // if correct word typed can update indexes as expected
                 setChar(char+1)
                 setCorrectChars(correctChars+1)
                 setLetterIndex(letterIndex+1)
                 setIndex(index+1)
-                event.target.value = ""
+                event.target.value = "" // update form to hold an empty string ready for user to type next word
             } else {
-                // make the thing that was skipped red
+                // if incorrect word typed means either part of the word wrong or some letters skipped - premature spacebar
                 const wordTyped = event.target.value.trim();
                 const previousLengths = getPreviousLengths(words, index)
+                // set all incorrect characters to red
                 for (let li = previousLengths+wordTyped.length; li < previousLengths + words[index].length; li++) {
                     console.log(li)
                     if (letters[li] !== " ") { 
-                        lettersEls[li] = <div className="text-red" style={{color: "red"}}>{letters[li]}</div>
+                        lettersEls[li] = <div className="text-red text-lg" style={{color: "red"}}>{letters[li]}</div>
                     }
                 }
                 setChar(char+1)
-                // setCorrectChars(correctChars+1)
                 setLetterIndex(getPreviousLengths(words, index+1))
                 setIndex(index+1)
-                event.target.value = ""
+                event.target.value = "" // update form for the same reason as stated above
             }
         } 
+
         else {
+            // get letter type from event.key
             const wordTyped = event.target.value.trim() + event.key
-            let letterTyped = wordTyped.charAt(letterIndex - getPreviousLengths(words, index)) // subtract the number of spaces 
-            letterTyped = event.key
-            // console.log(letterTyped, letters[letterIndex])
+            let letterTyped = event.key
+
+            // update colour of character to be typed depending on whether letter typed is the correct one (green or red)
             if (letters[letterIndex] !== " ") { 
                 if (letterTyped == letters[letterIndex]) {
                     setCorrectChars(correctChars+1)
-                    lettersEls[letterIndex] = <div className="text-green" style={{color: "green"}}>{letters[letterIndex]}</div>
+                    lettersEls[letterIndex] = <div className="text-green text-lg" style={{color: "green"}}>{letters[letterIndex]}</div>
                 } else {
-                    lettersEls[letterIndex] = <div className="text-red" style={{color: "red"}}>{letters[letterIndex]}</div>
+                    lettersEls[letterIndex] = <div className="text-red text-lg" style={{color: "red"}}>{letters[letterIndex]}</div>
                 }
             }
+
+            // update indexes by 1
             setChar(char+1)
             setLetterIndex(letterIndex+1)
-            // auto finish if the last word is right
-            // if (letterIndex === letters.length-1) {
-            //     event.target.value = ""
-            //     setIndex(index+1)
-            // }
         }
     }
 
+    useEffect(() => {
+        if (index === words.length) {
+            console.log("test done")
+            let finish = new Date()
+            setTimeTaken(finish.getTime() - startTime)
+        }
+    }, [index])
+
+    useEffect(() => {
+        const wordsTyped = correctChars/averageCharactersPerWord
+        const secondsTaken= timeTaken/1000 // timeTaken is in milliseconds
+        const acc= correctChars/char
+        console.log("Correct characters typed: " + correctChars)
+        console.log("Characters typed: " + char)
+        console.log("Accuracy: " + acc*100)
+        setWPM((wordsTyped/secondsTaken) * 60)
+        setAccuracy(acc*100)
+    }, [timeTaken])
+
     return (
         <div className="py-20 px-11 w-[95vw]">
-            <div className="flex flex-row flex-wrap">
-                {lettersEls.map((letterEl, idx) => {
-                    return (
-                        <div>
-                            {letterEl}
-                        </div>
-                    )
-                })}
+            <div className="flex justify-center">
+                <div className="flex flex-row flex-wrap">
+                    {lettersEls.map((letterEl, idx) => {
+                        return (
+                            <div>
+                                {letterEl}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-            <input onKeyDown={() => checkCorrect(event)} onKeyUp={() => checkDeleted(event)} 
-                    autoFocus className="rounded" type="text" tabIndex={0} 
+            <div className="flex-col my-[5vh] flex w-[100vw] items-center justify-center">
+                <input onKeyDown={() => checkCorrect(event)} onKeyUp={() => checkDeleted(event)} 
+                    autoFocus className="rounded text-black width" type="text" tabIndex={0} 
                     autoComplete="off" autoCapitalize="off" autoCorrect="off" 
                     data-gramm="false" data-gramm_editor="false" data-enable-grammarly="false" list="autocompleteOff"/>
+                <button className="mt-[5vh] text-white text-3xl" onClick={() => location.reload()}>⟳</button>
+            </div>
+            {(wpm > 0) 
+                ? <div className="flex w-[95vw] justify-around">
+                    <div className="w-[30vw] flex flex-col justify-center items-center">
+                        <h3 className="font-bold text-2xl mb-2">words per minute</h3>
+                        <p>{Math.round(wpm*10)/10}</p>
+                    </div>
+                    <div className="w-[30vw] flex flex-col justify-center items-center">
+                        <h3 className="font-bold text-2xl mb-2">accuracy</h3>
+                        <p>{Math.round(accuracy*10)/10}%</p>
+                    </div>
+                </div>
+                :
+                <div></div>
+            }
         </div>
     )
 }
